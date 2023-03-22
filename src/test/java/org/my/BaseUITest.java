4b4c4a576c7d6daf15e70a15f48b0587666a5bca
Anton <Anton.Bondarev@sabregames.com>
2023-03-22 17:53:26 +0200

    removed unnecessary imports

--- src/test/java/org/my/BaseUITest.java
+++ src/test/java/org/my/BaseUITest.java
@@ -3,10 +3,7 @@ package org.my;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.ElementClickInterceptedException;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
